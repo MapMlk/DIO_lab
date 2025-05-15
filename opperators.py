3a2ff53a323100cac009a84e06cbb5ee3e31d8f1
MapMlk <macella.mapmap@gmail.com>
2025-05-15 14:00:40 -0300

    operadores até maior ou igual à

--- opperators.py
+++ opperators.py
@@ -1,1 +1,51 @@
-print("hello world")
+# Exercicios do modulo "Operadores com python" da DIO
+result = 0
+
+## Operadores Aritméticos
+###Adição
+result = 2+3
+print("2 + 3 =", result)
+
+###Subtração
+result = (9-7)
+print("9 - 7 =", result)
+
+###Multiplicação
+result = 3*5
+print("3 x 5 =", result)
+
+###Divisão
+####Divisão normal
+result = 7/2
+print("Decimal", end=" ")
+print("7 / 2 =", result)
+####Divisão inteira
+result = 7//2
+print("Inteira", end=" ")
+print("7 / 2 =", result)
+####Modulo
+result = 7%2
+print("Modulo", end=" ")
+print("7 % 2 =", result)
+
+###Exponenciação
+result = 2**3
+print("2 ^ 3 =", result)
+###Raiz quadrada
+result = 9**(1/2)
+print("Raiz de 9 =", result)
+
+## Operadores de Comparação
+result = 2
+###Maior que
+print(result, "> 1? ", result>1)
+print(result, "> 4? ", result>4)
+
+###Menor que
+print(result, "< 1? ", result>1)
+print(result, "< 4? ", result<4)
+
+###Maior ou igual a
+print(result, ">= 1? ", result>=1)
+print(result, ">= 2? ", result>=2)
+print(result, ">= 4? ", result>=4)
